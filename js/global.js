787596663a767f0055365dd0e1d1569cee37b4d4
Yves91700 <yvespierre91700@gmail.com>
2022-10-15 23:38:14 +0200

    js modal portfolio

--- js/global.js
+++ js/global.js
@@ -62,4 +62,31 @@ tabsFilters();
 
 function showProjetDetails() {
 
+    const links = document.querySelectorAll(".card_link");
+    const modals = document.querySelectorAll(".modal");
+    const btns = document.querySelectorAll(".modal_close");
+
+    const hideModals = () => {
+        modals.forEach(modal => {
+            modal.classList.remove('show');
+
+        });
+    }
+
+links.forEach(elem => {
+    elem.addEventListener('click', (event)=> {
+        event.preventDefault();
+        document.querySelector(`[id=${elem.dataset.id}]`).classList.add('show');
+
+    });
+});
+
+
+btns.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+     hideModals();  
+    });
+});
+    
 }
+showProjetDetails();
